fb29766c6f4c5a8929a2d2dc8d221ff86248f9dc
Alfred Lee <2953554+justalfred@users.noreply.github.com>
2023-01-23 17:05:12 -0500

    update copyright years

--- keyboards/adafruit/ano_scroll_wheel/ano.h
+++ keyboards/adafruit/ano_scroll_wheel/ano.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 Alfred Lee
+/* Copyright 2023 Alfred Lee
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
